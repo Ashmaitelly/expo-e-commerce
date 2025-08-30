cf3dd7f72d1acf2f079aeca79736a5a67b456c87
Abdullah Shmaitelly <ashmaitelly@gmail.com>
2025-08-30 07:48:49 +0300

    Added redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,44 @@
-import { Assets as NavigationAssets } from '@react-navigation/elements';
-import { DarkTheme, DefaultTheme } from '@react-navigation/native';
-import { Asset } from 'expo-asset';
-import { createURL } from 'expo-linking';
-import * as SplashScreen from 'expo-splash-screen';
-import * as React from 'react';
-import { useColorScheme } from 'react-native';
-import { Navigation } from './navigation';
+import { Assets as NavigationAssets } from "@react-navigation/elements";
+import { DarkTheme, DefaultTheme } from "@react-navigation/native";
+import { Asset } from "expo-asset";
+import { createURL } from "expo-linking";
+import * as SplashScreen from "expo-splash-screen";
+import * as React from "react";
+import { useColorScheme } from "react-native";
+import { Navigation } from "./navigation";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 Asset.loadAsync([
   ...NavigationAssets,
-  require('./assets/newspaper.png'),
-  require('./assets/bell.png'),
+  require("./assets/newspaper.png"),
+  require("./assets/bell.png"),
 ]);
 
 SplashScreen.preventAutoHideAsync();
 
-const prefix = createURL('/');
+const prefix = createURL("/");
 
 export function App() {
   const colorScheme = useColorScheme();
 
-  const theme = colorScheme === 'dark' ? DarkTheme : DefaultTheme
+  const theme = colorScheme === "dark" ? DarkTheme : DefaultTheme;
 
   return (
-    <Navigation
-      theme={theme}
-      linking={{
-        enabled: 'auto',
-        prefixes: [prefix],
-      }}
-      onReady={() => {
-        SplashScreen.hideAsync();
-      }}
-    />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Navigation
+          theme={theme}
+          linking={{
+            enabled: "auto",
+            prefixes: [prefix],
+          }}
+          onReady={() => {
+            SplashScreen.hideAsync();
+          }}
+        />
+      </PersistGate>
+    </Provider>
   );
 }
---
+++ src/redux/store.ts
@@ -0,0 +1,28 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistReducer, persistStore } from "redux-persist";
+import userSlice from "./slices/userSlice";
+
+const rootReducer = combineReducers({
+  user: userSlice,
+});
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  whitelist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof rootReducer>;
